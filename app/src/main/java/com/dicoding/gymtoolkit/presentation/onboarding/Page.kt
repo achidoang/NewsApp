4a85b6391973d5945ab5a94be299dfe5d0d87837
muhammad aji <muhammadaji1614@gmail.com>
2024-01-21 17:56:36 +0700

    clear file tidak terpakai

--- app/src/main/java/com/dicoding/gymtoolkit/presentation/onboarding/Page.kt
+++ app/src/main/java/com/dicoding/gymtoolkit/presentation/onboarding/Page.kt
@@ -14,19 +14,19 @@ val pages = listOf(
     Page(
         title = "Stay Informed Anytime, Anywhere",
         description = "Welcome to NewsHub, your go-to app for the latest and most reliable news. Explore a world of information at your fingertips and stay connected with the stories that matter most to you.",
-        image = R.drawable.onboarding4 //image onboarding
+        image = R.drawable.onboarding4
     ),
 
     Page(
         title = "Dive Into Unparalleled News Coverage",
         description = "NewsHub offers unlimited access to top-notch journalism, expert analysis, and a diverse range of news categories. Join a community of informed readers and make every moment an opportunity to discover and understand the world around you.",
-        image = R.drawable.onboarding5 //image onboarding
+        image = R.drawable.onboarding5
     ),
 
     Page(
         title = "Smart Features for Your News Lifestyle",
         description = "Experience innovative features that let you customize your news feed, track real-time updates, and engage with stories that resonate with you. Embrace a new way of staying informed and welcome a healthier understanding of the world.",
-        image = R.drawable.onboarding6 //image onboarding
+        image = R.drawable.onboarding6
     )
 
 )
